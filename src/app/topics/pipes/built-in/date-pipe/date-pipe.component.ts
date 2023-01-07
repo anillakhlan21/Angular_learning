45e649ec7f912bb3f0ca98de51a95c77455e7ecf
Anil kumar <anil.kumar@altorumleren.com>
2023-01-07 12:21:08 +0530

    minor change

--- src/app/topics/pipes/built-in/date-pipe/date-pipe.component.ts
+++ src/app/topics/pipes/built-in/date-pipe/date-pipe.component.ts
@@ -10,8 +10,11 @@ registerLocaleData(localeEn);
 export class DatePipeComponent implements OnInit {
   toDate: Date = new Date('04/06/2020 13:05:09');
   formattedFromComponent: string | null='';
+
   constructor(private _datePipe: DatePipe) {
-    this.formattedFromComponent = this._datePipe.transform(new Date(this.toDate),'yyyy-MM-dd')
+    this.formattedFromComponent = this._datePipe.transform(this.toDate,'yyyy-MM-dd')
+    // this.formattedFromComponent = this._datePipe.transform(new Date(this.toDate),'yyyy-MM-dd')
+
   }
 
   ngOnInit(): void {
